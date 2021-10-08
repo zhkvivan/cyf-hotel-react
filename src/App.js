167c2caeaa33a1ce9a1d56b94481b5388cc403fc
Ivan Zhukov <zhkvivan@gmail.com>
2021-10-08 22:55:10 +0100

    lesson 1 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 import Bookings from "./Bookings";
+import Header from "./Heading";
+import TouristInfoCards from "./TouristInfoCards.js";
+import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Header />
+      <TouristInfoCards />
       <Bookings />
+      <Footer />
     </div>
   );
 };
