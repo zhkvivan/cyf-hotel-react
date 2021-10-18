4d9dcf45802e98b3a11ccfc2201b2cf1cb6f634f
Ivan Zhukov <zhkvivan@gmail.com>
2021-10-18 10:18:29 +0100

    last task in progress

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,21 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "./data/fakeBookings.json";
+// import FakeBookings from './data/fakeBookings.json';
 
 const Bookings = () => {
   const search = searchVal => {
+    setBookings(initialBookings);
     console.info("TO DO!", searchVal);
+    let filteredBookings = initialBookings.filter(booking => {
+      if (
+        booking.firstName.toLowerCase() == searchVal.toLowerCase() ||
+        booking.surname.toLowerCase() == searchVal.toLowerCase()
+      ) {
+        return true;
+      }
+    });
+    setBookings(filteredBookings);
   };
 
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  let [initialBookings, setInitialBookings] = useState([]);
+
+  const [isStillLoading, setIsStillLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/error`)
+      .then(res => res.json())
+      .then(data => {
+        setInitialBookings(data);
+        setBookings(data);
+        setIsStillLoading(false);
+      })
+      .catch(error => {
+        console.log("error", error);
+        setError(true);
+      });
+  }, []);
 
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults searchResults={bookings} />
-        {/* <SearchResults /> */}
+        {/* {bookings.length === 0 ? (
+					'Loading...'
+				) : (
+					<SearchResults searchResults={bookings} />
+				)} */}
+        {isError ? "error" : "no error"}
+        {/* {isStillLoading ? (
+					'Loading...'
+				) : (
+					<SearchResults searchResults={bookings} />
+				)} */}
       </div>
     </div>
   );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import moment from "moment";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = ({ searchResults }) => {
   const [rowBackgrounds, setRowBackgrounds] = useState([]);
@@ -11,48 +12,68 @@ const SearchResults = ({ searchResults }) => {
     });
   };
 
+  const [userId, setUserId] = useState(null);
+
+  const showProfileBtn = id => {
+    setUserId(id);
+  };
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Title</th>
-          <th scope="col">First name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room ID</th>
-          <th scope="col">Check in date</th>
-          <th scope="col">Check out date</th>
-          <th scope="col">Number of nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {searchResults.map((person, index) => {
-          let values = Object.values(person);
-          values.shift();
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">First name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room ID</th>
+            <th scope="col">Check in date</th>
+            <th scope="col">Check out date</th>
+            <th scope="col">Number of nights</th>
+            <th scope="col">Show profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map((person, index) => {
+            let values = Object.values(person);
+            values.shift();
 
-          let dateIn = moment(values[5]);
-          let dateOut = moment(values[6]);
-          let numberOfDays = dateOut.diff(dateIn, "days");
+            let dateIn = moment(values[5]);
+            let dateOut = moment(values[6]);
+            let numberOfDays = dateOut.diff(dateIn, "days");
 
-          return (
-            <tr
-              key={index}
-              onClick={() => {
-                highlightRow(index);
-              }}
-              className={
-                rowBackgrounds[index] ? "selectedRow" : "unSelectedRow"
-              }
-            >
-              {values.map((item, index) => {
-                return <td key={index}>{item}</td>;
-              })}
-              <td>{numberOfDays}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                key={index}
+                onClick={() => {
+                  highlightRow(index);
+                }}
+                className={
+                  rowBackgrounds[index] ? "selectedRow" : "unSelectedRow"
+                }
+              >
+                {values.map((item, index) => {
+                  return <td key={index}>{item}</td>;
+                })}
+                <td>{numberOfDays}</td>
+                <td>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                      showProfileBtn(person.id);
+                    }}
+                  >
+                    Show profile
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {userId ? <CustomerProfile id={userId} /> : ""}
+    </div>
   );
 };
 
